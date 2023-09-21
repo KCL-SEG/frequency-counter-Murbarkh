456cbd4bc364c3c846e3936e8140373e9306a394
Murbarkh <murbarkh.hamid@gmail.com>
2023-09-21 12:09:16 +0100

    Add frequencies function

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,17 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+
+    for values in items:
+        key = str(values)
+
+        if key in frequencies.keys():
+            frequencies[key] += 1
+        else:
+            frequencies[key] = 1
+
+
     return frequencies
+
+
+print(frequencies([100, 'Hello', '100', '100', 100]))
